327970e89339440280814ee336863a8b97e4f2ad
nikolayli <niko.zig@hotmail.com>
2025-02-19 14:24:02 +0300

    leetcode

--- tests/s0001_two_sum_test.rs
+++ tests/s0001_two_sum_test.rs
@@ -2,7 +2,7 @@ use leetcode::solutions::s0001_two_sum::Solution;
 
 #[test]
 fn test_two_sum() {
-    assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![1, 2]);
+    assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
     assert_eq!(Solution::two_sum(vec![3, 2, 4], 6), vec![1, 2]);
     assert_eq!(Solution::two_sum(vec![3, 3], 6), vec![0, 1]);
 }
